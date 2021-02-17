beddcaf940c7d164fe7fa1ff78848b80527a3cf8
Svetlana <89111378147@yandex.ru>
2021-02-18 00:10:25 +0300

    02

--- test_addnew.py
+++ test_addnew.py
@@ -15,7 +15,9 @@ class TestAddNew(unittest.TestCase):
     
     def test_add_new(self):
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -24,15 +26,20 @@ class TestAddNew(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open new page and init new creation
         wd.find_element_by_link_text("add new").click()
+        # fill new form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Fname")
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys("Lname")
+        # submit new creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        # return to home page
         wd.find_element_by_link_text("home").click()
+        # logout
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
