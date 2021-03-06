d16e1f65499792e334dd65d2447540a25d042ade
Svetlana <89111378147@yandex.ru>
2021-03-07 01:45:35 +0300

    0703

--- application.py
+++ application.py
@@ -56,5 +56,3 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
-
--- test_addnew.py
+++ test_addnew.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from new import New
-from fixture.application import Application
-
+from application import Application
 
 
 @pytest.fixture
@@ -10,23 +9,15 @@ def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-    
+
+
 def test_add_new(app):
     app.login(password="secret", username="admin")
     app.create_group(New(Surname="Lname", name="Fname"))
     app.logout()
 
+
 def test_add_emptynew(app):
     app.login(password="secret", username="admin")
     app.create_group(New(Surname="", name=""))
     app.logout()
-
-
-
-
-
-    
-
-    
-
-
