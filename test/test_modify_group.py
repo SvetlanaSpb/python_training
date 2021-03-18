bc351817766ad3e3a25c924585858fae584b6d15
Svetlana <89111378147@yandex.ru>
2021-03-19 00:22:03 +0300

    Оптимизация ввода данных в формы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,4 +1,11 @@
-def test_modify_first_group(app):
+from model.group import Group
+
+def test_modify_group_name(app):
     app.session.login(password="secret", username="admin")
-    app.group.modify_first_group()
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(password="secret", username="admin")
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
