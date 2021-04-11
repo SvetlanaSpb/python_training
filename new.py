b28464e226acbdd9e7982d73fe0608d5a80698d0
Svetlana <89111378147@yandex.ru>
2021-04-11 18:54:20 +0300

    failed test del contact

--- new.py
+++ new.py
@@ -4,3 +4,9 @@ class New:
         self.surname = surname
         self.name = name
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
--- test/test_del_new.py
+++ test/test_del_new.py
@@ -7,7 +7,8 @@ def test_delete_first_new(app):
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_new()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
-
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts [0:1] = []
+    assert old_contacts == new_contacts
 
 
