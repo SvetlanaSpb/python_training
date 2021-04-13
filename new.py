38adaf45f74169744b89d0cb5529f5b3d4c26e9c
Svetlana <89111378147@yandex.ru>
2021-04-13 08:58:10 +0300

    Проверка равенства объектов и списков_контакты

--- new.py
+++ new.py
@@ -8,4 +8,10 @@ class New:
         self.name = name
         self.id = id
 
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
+
 
