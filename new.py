af3b2cd10a245160164f749e19b6c933daa321f2
Svetlana <89111378147@yandex.ru>
2021-04-16 13:53:45 +0300

    test failed

--- new.py
+++ new.py
@@ -12,4 +12,4 @@ class New:
         return "%s:%s" % (self.id, self.name)
 
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
