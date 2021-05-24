57093927d282186308938dbeef1ae7935536b0b0
Svetlana <89111378147@yandex.ru>
2021-05-24 23:28:38 +0300

    Реализованы проверки в виде сравнения списков

--- test/test_modify_new.py
+++ test/test_modify_new.py
@@ -5,9 +5,14 @@ def test_modify_first_new(app):
     if app.contact.count() == 0:
         app.contact.create(New(surname="test", name="test"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_new(New(surname="Lname", name="Fname"))
+    contact = New(surname="Lname", name="Fname")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_new(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=New.id_or_max) == sorted(new_contacts, key=New.id_or_max)
+
 
 
 
