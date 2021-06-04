0fc5b390a46724e5ca3419b2b2514ab318c872cf
Svetlana <89111378147@yandex.ru>
2021-06-04 23:33:33 +0300

    Улучшили функцию чтения информации о пользователе из тестируемого приложения

--- fixture/session.py
+++ fixture/session.py
@@ -32,7 +32,13 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        # из браузера читаем имя пользователя
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        # делаем вырезку [1:-1]
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text[1:-1]
 
     def ensure_login(self, password, username):
         wd = self.app.wd
