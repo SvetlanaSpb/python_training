e434c976a5cd3e00cb5c2c0a0d4aa51f06d9a168
Svetlana <89111378147@yandex.ru>
2021-06-09 01:03:00 +0300

    Cравнениt телефонов

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,73 +1,117 @@
+from selenium.webdriver.support.ui import Select
 from new import New
+import re
+
 
 class ContactHelper:
 
     def __init__(self, app):
         self.app = app
 
-    def return_to_home_page(self):
-         wd = self.app.wd
-         wd.find_element_by_link_text("home").click()
-
-    def delete_first_new(self):
-        self.delete_contact_by_index(0)
-
-    def delete_contact_by_index(self, index):
-        wd = self.app.wd
-        self.app.open_home_page()
-        # init contact deletion
-        wd.find_elements_by_name("selected[]")[index].click()
-        wd.find_element_by_xpath("//input[@value='Delete']").click()
-        # fill contact form
-        wd.switch_to_alert().accept()
-        wd.find_element_by_css_selector("div.msgbox")
-        self.return_to_home_page()
-        self.contact_cache = None
-
-    def select_first_new(self):
-        wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
-
-
-    def modify_first_new(self):
-        # один метод реализуется через другой
-        self.modify_contact_by_index(0)
-
-    def modify_contact_by_index(self, index,new):
-        wd = self.app.wd
-        self.app.open_home_page()
-        wd.find_elements_by_name("selected[]")[index].click()
-        wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
-        self.fill_contact_forms(new)
-        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
-        self.return_to_home_page()
-        self.contact_cache = None
-
-    def create(self, new):
+    def add_contact(self, contact):
         wd = self.app.wd
         self.open_home_page()
+        # init contact creation
         wd.find_element_by_link_text("add new").click()
-        self.fill_contact_forms(new)
-        # submit new creation
+        # fill contact firm
+        self.fill_contact_forms(contact)
+        # submit group creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.return_to_home_page()
         self.contact_cache = None
 
-    def fill_contact_forms(self, new):
+    def edit_contact_by_index(self, index, contact):
         wd = self.app.wd
-        # fill new form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(new.name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(new.surname)
+        self.open_home_page()
+        wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
+        # edit contact firm
+        self.fill_contact_forms(contact)
+        # submit contact update
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+        self.contact_cache = None
+
+    def change_field_value(self, field_name, text):
+         wd = self.app.wd
+         if text is not None:
+             wd.find_element_by_name(field_name).click()
+             wd.find_element_by_name(field_name).clear()
+             wd.find_element_by_name(field_name).send_keys(text)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def select_first_contact(self):
+        wd = self.app.wd
+        self.open_home_page()
+        wd.find_element_by_name("selected[]").click()
+
+
+    def delete_contact_by_index(self, index):
+        wd = self.app.wd
+        self.open_home_page()
+        # select first contact
+        wd.find_elements_by_name("selected[]")[index].click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def open_home_page(self):
         wd = self.app.wd
         if not (wd.current_url.endswith("http://localhost/addressbook/") and len(wd.find_elements_by_name("add")) > 0):
             wd.get("http://localhost/addressbook/")
 
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+
+    def fill_contact_forms(self, contact):
+        wd = self.app.wd
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("middlename", contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+
+        self.change_field_value("title", contact.title)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("address", contact.address)
+        self.change_field_value("home", contact.home)
+        self.change_field_value("mobile", contact.mobile)
+        self.change_field_value("work", contact.work)
+        self.change_field_value("fax", contact.fax)
+        self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3", contact.email3)
+        self.change_field_value("homepage", contact.homepage)
+
+        self.change_field_value_date("bday", contact.bday)
+        self.change_field_value_date("bmonth", contact.bmonth)
+        self.change_field_value("byear", contact.byear)
+
+        self.change_field_value_date("aday", contact.aday)
+        self.change_field_value_date("amonth", contact.amonth)
+        self.change_field_value("ayear", contact.ayear)
+
+        self.change_field_value("address2", contact.address2)
+        self.change_field_value("phone2", contact.phone2)
+        self.change_field_value("notes", contact.notes)
 
     def count(self):
         wd = self.app.wd
@@ -76,22 +120,72 @@ class ContactHelper:
 
     contact_cache = None
 
+
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
             self.open_home_page()
-        # строим объект с информацией о контакте contacts = []
             self.contact_cache = []
             for row in wd.find_elements_by_name("entry"):
                 cells = row.find_elements_by_tag_name("td")
-            # получаем данные из ячеек
-                name = cells[2].text
-                surname = cells[1].text
-                contact_id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-            # параметры для конструирования нового объекта contacts
-                self.contact_cache.append(New(id=contact_id, surname=surname, name=name))
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                #так как в ячейке телефонов отдельные телефоны не указаны приходится получать информацию по всей ячейке а потом порезать её на части
+                all_phones = cells[5].text #теперь это список телефонов у ячейки берём текст а потом делим его на телефоны
+                # и мы можем этот список использовать что бы заполнить свойства объекта contact
+                all_emails = cells[4].text
+                self.contact_cache.append(
+                    New(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones,
+                            all_emails_from_home_page=all_emails))
+
+
         return list(self.contact_cache)
 
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        #открываем форму редактирования по заданному индексу
+        self.open_contact_to_edit_by_index(index)
+        #из формы читаем информацию
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")#текст который мы видим в поле является значением аттрибута value
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        homephone = wd.find_element_by_name("home").get_attribute("value")
+        workphone = wd.find_element_by_name("work").get_attribute("value")
+        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
+        # Строим объект из полученных данных, сначала название параметра а потом название локальной переменной
+        return New(firstname=firstname, lastname=lastname, homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone,
+                       id=id, email=email, email2=email2, email3=email3)
+
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        #вытаскиваем текст
+        text = wd.find_element_by_id("content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        secondaryphone = re.search("P: (.*)", text).group(1)
+
+        return New(homephone=homephone, mobilephone=mobilephone, workphone=workphone, secondaryphone=secondaryphone)
+
+    def change_field_value_date(self, field_date, text):
+        wd = self.app.wd
+        if text is not None:
+            selector = Select(wd.find_element_by_name(field_date))
+            selector.select_by_visible_text(text)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
 
 
