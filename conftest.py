a14ea5b1efd59e57c6bc40a0b41e29f89326e160
Svetlana <89111378147@yandex.ru>
2021-06-25 07:20:33 +0300

    Создан конфигурационный файл для описания тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 import pytest
 from fixture.application import Application
+import json
 
+# определяем глобальную переменную
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
+    #указываем что собираеися использовать глобальную переменную
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
+    #пишем проверку
+    if target is None:
+    #читаем файл джейсон
+        with open(request.config.getoption("--target")) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
         #функция которая инициализирует фикстуру создает объект класса aplication
         #при инициализации фикстуры нужно передавать параметр
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(password="secret", username="admin")
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -32,5 +38,6 @@ def stop(request):
 def pytest_addoption(parser):
     #если параметр браузер не указан то по дефолту будет запускаться фаерфокс
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    #хранить конфигурацию будем в формате джейсн
+    parser.addoption("--target", action="store", default="target.json")
 
